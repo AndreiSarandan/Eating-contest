4df0ae7263eb16db5d8f8d12710dc32f587ec6bb
AndreiSarandan <80057863+AndreiSarandan@users.noreply.github.com>
2022-08-17 11:53:02 +0300

    Update WhoAteWhat.py

--- WhoAteWhat.py
+++ WhoAteWhat.py
@@ -22,3 +22,7 @@ def scoreboard(who_ate_what):
 
     final = sorted(li, key=lambda d: (-d['score'], d['name']))
     return(final)
+
+
+print(scoreboard([{'name': 'T800'}, {'name': 'T-X', 'hotdogs': 11, 'chickenwings': 2, 'hamburgers': 11}, {'name': 'Skynet', 'hamburgers': 15, 'chickenwings': 8, 'hotdogs': 6},
+      {'name': 'T1000', 'chickenwings': 5}, {'name': 'Sarah Connor', 'hamburgers': 15, 'hotdogs': 3}, {'name': 'John Connor', 'chickenwings': 13}, {'name': 'Kyle Reese', 'hotdogs': 10}]))
